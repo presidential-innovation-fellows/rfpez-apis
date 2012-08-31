0d22814920b8da6046c1fd7607965a0092e9b564
adamjacobbecker <ad@mbecker.cc>
2012-08-31 10:29:19 -0400

    fixing update_from_foia task

--- update_from_foia.sh
+++ update_from_foia.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 wget http://ultra3d.sba.gov/foia/foia.tar.gz
-tar -C foia/dump  -zxvf foia.tar.gz
-node foia/tasks/import.js
+tar -C _data/dsbs  -zxvf foia.tar.gz
+node tasks/import_bizs.js
