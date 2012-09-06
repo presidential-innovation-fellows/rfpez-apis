526b0e1f34966ca616d8b5687f60b55c41d321ac
adamjacobbecker <ad@mbecker.cc>
2012-09-06 12:20:03 -0400

    specify per_page for exclusions as well.

--- controllers/exclusion.js
+++ controllers/exclusion.js
@@ -3,7 +3,8 @@ var Exclusion = require('../models/exclusion');
 exports.index = function(req, res) {
 
   var page = parseInt(req.query.page, 10) || 1;
-  var perPage = 10;
+  var perPage = parseInt(req.query.per_page) || 10;
+  if (perPage > 100) perPage = 100;
   var query = Exclusion.apiQuery(req.query).limit(perPage).skip((page-1)*perPage); //.sort('name');
   var response = {};
 
