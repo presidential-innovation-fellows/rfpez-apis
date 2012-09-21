2a54a9e3c3245ce763af69c8e0e11d3a8e8c18b9
Jed Wood <jed@limechile.com>
2012-09-21 16:12:21 -0400

    set trust proxy in order to get end user's real IP address

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.MONGOHQ_URL) {
 }
 
 var app = express();
-
+app.set("trust proxy", true);
 app.use(function(req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
